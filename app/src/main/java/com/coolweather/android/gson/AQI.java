7bdf7d40f332362c2666183a89123f9f90c61316
Tony <tony@gmail.comgit config --globalgit config --global user.email tony@gmail.com>
2017-09-25 11:13:38 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by ws on 2017/9/23.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
